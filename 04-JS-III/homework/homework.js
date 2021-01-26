26883db51a12c5b1ca9d4b5c6c831bed2392e00f
JainerJ <jainerj@uninorte.edu.co>
2021-01-25 22:00:41 -0500

    Homework 6 done

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -164,8 +164,6 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  var str;
-  str = n;
   if(n.toString()[0] == 9)
     return true;
   else
